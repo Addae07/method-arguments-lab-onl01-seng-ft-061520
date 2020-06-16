5042cd7036c86805ddec0ddc13f767467696ec2e
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-16 11:51:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def greeting_programmer(name, language)
+def #introduction_with_language(name, language)
   puts "Hello, #{name}. We heard you are a great #{language} programmer."
 end
